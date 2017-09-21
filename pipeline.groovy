eb4ffaae47b49a6d12c25a7cbdcedf59f39c12bc
Ben Hosmer <ben.hosmer@gmail.com>
2017-09-21 15:17:21 -0400

    Fix unable to import

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,26 +1,55 @@
 #!groovy
 
-job('Beachfront Seed') {
-    triggers {
-      upstream('example-1', 'UNSTABLE')
-    }
-    steps {
-        dsl {
-            external('jobs/example-1/Beachfront.groovy')
-            removeAction('DISABLE')
-        }
-    }
+import groovy.io.FileType
+import static Piazza.pzprojects
+import static Piazza.pzparams
+import static Piazza.pzcredsparams
+
+println pzparams
+println pzprojects
+println pzcredsparams
+def foldername = 'hosmer-snowflakes/yorkshire'
+def root = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
+//evaluate(new File("${root}/beachfront.properties"))
+//evaluate(new File("${root}/piazza.properties"))
+
+folder("${foldername}/piazza") {
+  displayName("piazza")
 }
 
-job('Piazza Seed') {
-    triggers {
-      upstream('example-1', 'UNSTABLE')
-    }
-    steps {
-        dsl {
-            external('jobs/example-1/Piazza.groovy')
-            removeAction('DISABLE')
-        }
-    }
-}
+def gitprefix = 'https://github.com/venicegeo/'
 
+for(project in pzprojects) {
+  pipelineJob("${foldername}/piazza/${project.name}-pipeline") {
+    description("Piazza pipeline")
+    triggers {
+      gitHubPushTrigger()
+    }
+    environmentVariables {
+      env('THREADFIX_ID', project.threadfixId)
+    }
+    definition {
+      cpsScm {
+        scm {
+          git {
+            remote {
+              url("${gitprefix}${project.name}")
+              branch("*/master")
+            }
+          }
+        }
+      }
+    }
+    parameters {
+      for(param in pzparams) {
+        "${param.type}"("${param.name}", "${param.defaultVaue}", "${param.description}")
+      }
+      for(credsparam in pzcredsparams) {
+        "${credsparam.type}"("${credsparam.name}") {
+          defaultValue("${credsparam.defaultValue}")
+          description("${credsparam.description}")
+        }
+      }
+    }
+  }
+}
