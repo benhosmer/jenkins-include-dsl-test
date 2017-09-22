493184a6f2040275c197a5291f6f035fa556715a
Ben Hosmer <ben.hosmer@gmail.com>
2017-09-22 12:17:08 -0400

    Update

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,7 @@
 #!groovy
 
-import groovy.io.FileType
-
 def foldername = 'hosmer-snowflakes/yorkshire'
-def root = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
 
-String props = readFileFromWorkspace("piazza.properties")
-configs = new ConfigSlurper().parse( props )
-println configs
+String piazzaprops = readFileFromWorkspace("piazza.properties")
+piazzaconfigs = new ConfigSlurper().parse( props )
+println piazzaconfigs.pzprojects
