fce8a18ae2698b4a03012a97ed9198776a767e01
Ben Hosmer <ben.hosmer@gmail.com>
2017-09-22 12:00:23 -0400

    Try yaml instead

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,46 +7,9 @@ def root = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
 //evaluate(new File("${root}/beachfront.properties"))
 //evaluate(new File("${root}/piazza.properties"))
 
-def jsonSlurper = new groovy.json.JsonSlurper()
-def piazza = jsonSlurper.parseText(readFileFromWorkspace('Piazza.json'))
+import org.yaml.snakeyaml.Yaml
 
-folder("${foldername}/piazza") {
-  displayName("piazza")
-}
+Yaml parser = new Yaml()
+List example = parser.load(("my.yaml" as File).text)
 
-def gitprefix = 'https://github.com/venicegeo/'
-
-for(project in pzprojects) {
-  pipelineJob("${foldername}/piazza/${project.name}-pipeline") {
-    description("Piazza pipeline")
-    triggers {
-      gitHubPushTrigger()
-    }
-    environmentVariables {
-      env('THREADFIX_ID', project.threadfixId)
-    }
-    definition {
-      cpsScm {
-        scm {
-          git {
-            remote {
-              url("${gitprefix}${project.name}")
-              branch("*/master")
-            }
-          }
-        }
-      }
-    }
-    parameters {
-      for(param in pzparams) {
-        "${param.type}"("${param.name}", "${param.defaultVaue}", "${param.description}")
-      }
-      for(credsparam in pzcredsparams) {
-        "${credsparam.type}"("${credsparam.name}") {
-          defaultValue("${credsparam.defaultValue}")
-          description("${credsparam.description}")
-        }
-      }
-    }
-  }
-}
+example.each{println it.subject}
